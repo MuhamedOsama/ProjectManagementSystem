80c1e510cdb7302e81149c908f02f19286c62777
Mohamed Hossam <mohamed.m201139@gmail.com>
2019-04-28 17:33:22 +0200

    Adding Users List

--- ProjectManagementSystem/Controllers/HomeController.cs
+++ ProjectManagementSystem/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using ProjectManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data;
+using System.Data.Entity;
 
 namespace ProjectManagementSystem.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         [Authorize]
         public ActionResult Index()
         {
@@ -21,9 +27,13 @@ namespace ProjectManagementSystem.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var userStore = new UserStore<User>(db);
+            var manager = new UserManager<User>(userStore);
+            var user = User.Identity;
+            var users = db.Users.Include(p => p.Roles);
+            var roles = manager.GetRoles(user.GetUserId());
+            return View("About", users.ToList());
+            
         }
 
         public ActionResult Contact()
